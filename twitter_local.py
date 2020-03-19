c2ccef38d29be3899377d53d1b43d2bd3d14e1e2
mick-tessari <miky.tex@gmail.com>
2020-03-19 21:29:26 +0100

    Adding multithreading and a 15-min timeout

--- twitter_local.py
+++ twitter_local.py
@@ -5,15 +5,19 @@
 # Future modules for Python 2
 from __future__ import print_function
 
+import json
+import os
+import queue
+
 # Built-in modules
 import sys
-import os
-import json
+import threading
+import time
+from datetime import datetime
 
 # Third-party modules
 import boto3
 import tweepy
-from datetime import datetime
 
 __author__ = "Michele Tessari"
 __version__ = "1.0"
@@ -21,71 +25,73 @@ __maintainer__ = "Michele Tessari"
 __email__ = "michele.tessari@snowflake.com"
 __status__ = "Prototype"
 
-# Twitter authentication for Tweepy
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
 
-# Global variables
-s3_client = boto3.client('s3')
-tweet_list = []
-total_count = 0
+def consumer():
+    tweet_list = []
+    total_count = 0
+    s3_client = boto3.client('s3')
 
-class JSONStreamProducer(tweepy.StreamListener):
+    while not event.is_set() or not pipeline.empty():
 
-    def __init__(self, s3_client):
-        super(tweepy.StreamListener, self).__init__()
-        self.s3_client = s3_client
-
-    def on_data(self, data):
-        global tweet_list
-        global total_count
+        data = pipeline.get()
         data_dict = json.loads(data)
-        data_dict.update({"keyword":keyword})
+        data_dict.update({"keyword": keyword})
         tweet_list.append(json.dumps(data_dict))
         total_count += 1
+
         if total_count % 2 == 0:
             print(".", end="")
-            sys.stdout.flush()
         if total_count % 100 == 0:
             print("{0} tweets retrieved".format(str(total_count)))
-            filename = 'tweets_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.json'
-            print("==> writing " + str(len(tweet_list)) + " records to " + filename)
+            filename = f"tweets_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+            print(f"==> writing {str(len(tweet_list))} records to {filename}")
             tweet_file = open(filename, 'w')
             tweet_file.writelines(tweet_list)
             tweet_file.close()
-            print("==> uploading file to S3...")
-            sys.stdout.flush()
             now = datetime.now()
-            key = "{0}/{1}/{2}/{3}/{4}/{5}/{6}".format(keyword, str(now.year), str(now.month), str(now.day),str(now.hour), str(now.minute), filename)
-            print("==> uploading to " + key)
+            key = "{0}/{1}/{2}/{3}/{4}/{5}/{6}".format(keyword, str(now.year), str(now.month), str(now.day),
+                                                       str(now.hour), str(now.minute), filename)
+            print(f"==> uploading to {key}")
             s3_client.upload_file(filename, bucket, key)
-            print("==> file uploaded to " + key)
+            print(f"==> uploaded to {key}")
             tweet_list = []
             os.remove(filename)
-        return True
+
+
+class JSONStreamProducer(tweepy.StreamListener):
+
+    def on_data(self, data):
+        pipeline.put(data)
+        if not event.is_set():
+            return True
+        else:
+            return False
 
     def on_error(self, status):
         print("Error: " + str(status))
 
 
-def main():
-    global bucket
-    global keyword
+if __name__ == "__main__":
     consumer_key = sys.argv[1]
     consumer_secret = sys.argv[2]
     access_token = sys.argv[3]
     access_token_secret = sys.argv[4]
     bucket = sys.argv[5]
-    keyword = "#"+sys.argv[6]
+    keyword = "#" + sys.argv[6]
 
-    global auth
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    mylistener = JSONStreamProducer(s3_client)
-    myStream = tweepy.Stream(auth=auth, listener=mylistener)
-    myStream.filter(track=[keyword])
+    pipeline = queue.Queue()
+    event = threading.Event()
 
+    myListener = JSONStreamProducer()
+    myStream = tweepy.Stream(auth=auth, listener=myListener)
 
-if __name__ == "__main__":
-    main()
+    t = threading.Thread(target=consumer)
+
+    myStream.filter(track=[keyword], is_async=True)
+    t.start()
+
+    time.sleep(900)
+    event.set()
